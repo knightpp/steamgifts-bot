6124ae00285b57e54d62ac373a407ca18e41695d
Daniel <knightpp@pm.me>
2020-01-07 23:35:42 +0200

    v0.4.0

--- SteamGiftAcc.cpp
+++ SteamGiftAcc.cpp
@@ -4,23 +4,26 @@
 
 #include "SteamGiftAcc.h"
 
-size_t SteamGiftAcc::WriteCallback(char *contents, size_t size, size_t nmemb, void *userp) {
-	((std::string *) userp)->append((char *)contents, size * nmemb);
+size_t SteamGiftAcc::WriteCallback(char *contents, size_t size, size_t nmemb, void *userp)
+{
+	((std::string *)userp)->append((char *)contents, size * nmemb);
 	return size * nmemb;
 }
 
-bool SteamGiftAcc::log_in(const string& phpsessid) {
+bool SteamGiftAcc::log_in(const string &phpsessid)
+{
 	static string loggedFlag = R"(<div class="nav__row is-clickable js__logout")";
-    static string xsrfFlag = R"(<input type="hidden" name="xsrf_token" value=")";
-    static string fundsFlag = R"(<span class="nav__points">)";
+	static string xsrfFlag = R"(<input type="hidden" name="xsrf_token" value=")";
+	static string fundsFlag = R"(<span class="nav__points">)";
 
 	this->phpsessidCookie = phpsessid;
 	string resp(std::move(get(SITEURL, phpsessidCookie)));
 	bool logged = resp.find(loggedFlag) != string::npos;
-	if (logged) {
+	if (logged)
+	{
 		size_t index = resp.find(xsrfFlag);
 		xsrf_token = string(resp.begin() + index + xsrfFlag.size(),
-			resp.begin() + index + xsrfFlag.size() + 32);
+							resp.begin() + index + xsrfFlag.size() + 32);
 		D("Logged: true\txsrf_token: " << xsrf_token);
 
 		index = resp.find(fundsFlag);
@@ -31,46 +34,55 @@ bool SteamGiftAcc::log_in(const string& phpsessid) {
 	return false;
 }
 
-SteamGiftAcc::SteamGiftAcc() : points(-1){
+SteamGiftAcc::SteamGiftAcc() : points(-1)
+{
 	clog.rdbuf(cout.rdbuf());
 	//clog << setfill(' ');
 }
 
-int SteamGiftAcc::parseInt(const string& str){
+int SteamGiftAcc::parseInt(const string &str)
+{
 	stringstream ss;
-	for (char i : str) {
-		if (isdigit(i)) 
+	for (char i : str)
+	{
+		if (isdigit(i))
 			ss << i;
 	}
 	if (ss.tellp() == 0)
-	    throw std::runtime_error("Tried to parse INT from STRING, but no INT was found.\nstr: " + str);
-		//return 0;
+		throw std::runtime_error("Tried to parse INT from STRING, but no INT was found.\nstr: " + str);
+	//return 0;
 	return stoi(ss.str());
 }
 
-ERROR SteamGiftAcc::enterGA(const GiveAway& ga){
-	if ((points - ga.price >= 0)) {
+MYERROR SteamGiftAcc::enterGA(const GiveAway &ga)
+{
+	if ((points - ga.price >= 0))
+	{
 		static string url(SITEURL + "/ajax.php");
-		string postfields(move("xsrf_token=" + xsrf_token+ "&do=entry_insert&" +"code=" + ga.getCode()));
+		string postfields(move("xsrf_token=" + xsrf_token + "&do=entry_insert&" + "code=" + ga.getCode()));
 		string resp(move(post(url, phpsessidCookie, postfields, ga)));
 		bool rez = resp.find("success") != string::npos;
-		if (!rez) {
-			if (resp.find("Previously Won") == string::npos) {
-				return ERROR::PREVIOUSLY_WON;
+		if (!rez)
+		{
+			if (resp.find("Previously Won") == string::npos)
+			{
+				return MYERROR::PREVIOUSLY_WON;
 			}
-			return ERROR::UNKNOWN;
+			return MYERROR::UNKNOWN;
 		}
-		else {
+		else
+		{
 			//points = points - ga.price;
 			auto index = resp.find("points");
 			points = parseInt(resp.substr(index, resp.size() - index));
-			return ERROR::OK;
+			return MYERROR::OK;
 		}
 	}
-	return ERROR::NOT_ENOUGH_POINTS;
+	return MYERROR::NOT_ENOUGH_POINTS;
 }
 
-string SteamGiftAcc::get(const string& url, const string& cookie) const{
+string SteamGiftAcc::get(const string &url, const string &cookie) const
+{
 	struct curl_slist *list = nullptr;
 	string resp;
 	CURL *curl;
@@ -81,7 +93,7 @@ string SteamGiftAcc::get(const string& url, const string& cookie) const{
 	list = curl_slist_append(list, "Accept-Language: en,en-US;q=0.8,uk;q=0.5,ru;q=0.3");
 	list = curl_slist_append(list, "Connection: close");
 	//list = curl_slist_append(list, "Connection: close");
-	list = curl_slist_append(list, ("Cookie: PHPSESSID="+cookie).c_str());
+	list = curl_slist_append(list, ("Cookie: PHPSESSID=" + cookie).c_str());
 	list = curl_slist_append(list, "DNT: 1");
 	list = curl_slist_append(list, "Host: www.steamgifts.com");
 	list = curl_slist_append(list, "Referer: www.steamgifts.com");
@@ -91,36 +103,36 @@ string SteamGiftAcc::get(const string& url, const string& cookie) const{
 
 	curl_global_init(CURL_GLOBAL_ALL);
 	curl = curl_easy_init();
-	if (curl) {
-
+	if (curl)
+	{
 		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
 		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
-	//	curl_easy_setopt(curl, CURLOPT_COOKIE, ("PHPSESSID="+cookie).c_str());
+		//	curl_easy_setopt(curl, CURLOPT_COOKIE, ("PHPSESSID="+cookie).c_str());
 		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
 		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
 		curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING, "");
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &resp);
-		
+
 		res = curl_easy_perform(curl);
 
 		if (res != CURLE_OK)
 			throw runtime_error(string(curl_easy_strerror(res)));
-
 	}
 	curl_easy_cleanup(curl);
 	//curl_global_cleanup();
 	return resp;
 }
 
-string SteamGiftAcc::post(const string& url, const string& cookie, const string& postfields, const GiveAway& ga) const{
+string SteamGiftAcc::post(const string &url, const string &cookie, const string &postfields, const GiveAway &ga) const
+{
 	struct curl_slist *list = nullptr;
 	string resp;
 	CURL *curl;
 	CURLcode res;
 	//Accept-Encoding: gzip, deflate, br ,......//////////////////////////////////////////////////
-    //	list = curl_slist_append(list, "Accept: application/json, text/javascript, */*; q=0.01");
+	//	list = curl_slist_append(list, "Accept: application/json, text/javascript, */*; q=0.01");
 	list = curl_slist_append(list, "Host: www.steamgifts.com");
 	list = curl_slist_append(list, USERAGENT);
 	list = curl_slist_append(list, "Accept: application/json, text/javascript, */*; q=0.01");
@@ -133,10 +145,11 @@ string SteamGiftAcc::post(const string& url, const string& cookie, const string&
 	list = curl_slist_append(list, ("Cookie: PHPSESSID=" + cookie).c_str());
 	list = curl_slist_append(list, "TE: Trailers");
 
-
 	curl_global_init(CURL_GLOBAL_ALL);
 	curl = curl_easy_init();
-	if (curl) {
+	if (curl)
+	{
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
 		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
 		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
 		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
@@ -154,7 +167,8 @@ string SteamGiftAcc::post(const string& url, const string& cookie, const string&
 	return resp;
 }
 
-void SteamGiftAcc::parseGiveaway(const string& url, GArray* array) const{
+void SteamGiftAcc::parseGiveaway(const string &url, GArray *array) const
+{
 	/// переписать нормально
 	using namespace htmlcxx;
 	HTML::ParserDom parser;
@@ -163,13 +177,16 @@ void SteamGiftAcc::parseGiveaway(const string& url, GArray* array) const{
 	tree<HTML::Node>::iterator it = dom.begin();
 	tree<HTML::Node>::iterator end = dom.end();
 
-	for (; it != end; ++it) {
-		if (it->tagName() == "div") {
+	for (; it != end; ++it)
+	{
+		if (it->tagName() == "div")
+		{
 			it->parseAttributes();
 			auto attr = it->attribute("class");
-			if (attr.first) {
-				if (attr.second.find("is-faded") == string::npos
-					&& attr.second.find("giveaway__row-inner-wrap") != string::npos) {
+			if (attr.first)
+			{
+				if (attr.second.find("is-faded") == string::npos && attr.second.find("giveaway__row-inner-wrap") != string::npos)
+				{
 					it += 6;
 					it->parseAttributes();
 
@@ -180,19 +197,22 @@ void SteamGiftAcc::parseGiveaway(const string& url, GArray* array) const{
 					it += 3;
 					it->parseAttributes();
 
-
-					if (it->text().find("Copies") != string::npos) {
+					if (it->text().find("Copies") != string::npos)
+					{
 						ga.copies = parseInt(it->text());
 
 						it += 2;
 						ga.price = parseInt(it->text());
 					}
-					else {
+					else
+					{
 						ga.price = parseInt(it->text());
 					}
-					while (it != end) {
+					while (it != end)
+					{
 						string text(it->text());
-						if (text.find(" entries") != string::npos) {
+						if (text.find(" entries") != string::npos)
+						{
 							ga.entries = parseInt(it->text());
 							break;
 						}
@@ -206,25 +226,29 @@ void SteamGiftAcc::parseGiveaway(const string& url, GArray* array) const{
 	D("Parsed url: " << url);
 }
 
-GArray SteamGiftAcc::parseGiveaways(int pageCount/* = 1*/, int pageStart/* = 1*/) const{
+GArray SteamGiftAcc::parseGiveaways(int pageCount /* = 1*/, int pageStart /* = 1*/) const
+{
 	GArray giveaways;
 	D("Parsing pages from: " << pageStart << " to " << pageStart + pageCount - 1);
 
 	giveaways.reserve(pageCount * 50);
 	static const string urlPage("https://www.steamgifts.com/giveaways/search?page=");
-	for (int i = pageStart; i < pageCount + pageStart; i++) {
+	for (int i = pageStart; i < pageCount + pageStart; i++)
+	{
 		parseGiveaway(urlPage + to_string(i), &giveaways);
-		if(pageCount > 1)
+		if (pageCount > 1)
 			std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_PAGE_PARSE_MS));
 	}
 	return giveaways;
 }
 
-SteamGiftAcc* SteamGiftAcc::getInstance(){
+SteamGiftAcc *SteamGiftAcc::getInstance()
+{
 	static SteamGiftAcc instance;
 	return &instance;
 }
 
-int SteamGiftAcc::getPoints() const {
-    return this->points;
+int SteamGiftAcc::getPoints() const
+{
+	return this->points;
 }
--- main.cpp
+++ main.cpp
@@ -1,138 +1,166 @@
-#include "SteamGiftAcc.h"
-#include <fstream>
 #include <cstdio>
-#include <exception>
-#include "cxxopts/include/cxxopts.hpp"
+#include <ctime>
 
-#ifdef WIN
+#include <chrono>
+#include <fstream>
+#include <exception>
+
+#include "cxxopts.hpp"
+#include "SteamGiftAcc.h"
+
+#ifdef WIN32
 #include <windows.h>
 #endif
 
 using namespace std;
 
-bool ReadFromFile(const string& path, string* lhs);
-bool WriteToFile(const string& path, const string& data);
+bool ReadFromFile(const string &path, string *lhs);
+bool WriteToFile(const string &path, const string &data);
 
 #define printFailFunction() printf("-> %s() failed\n", __FUNCTION__);
 
-int main(int argc, char** argv) {
-    printf("[Bild date] %s, %s\n", __DATE__, __TIME__);
+int fakeMain(int, char **);
+
+int main(int argc, char **argv)
+{
+    fakeMain(argc, argv);
+#ifdef WIN32
+    // cout << "Press ENTER...";
+    system("pause");
+#endif
+}
+
+int fakeMain(int argc, char **argv)
+{
+    printf("[Build date] %s, %s\n", __DATE__, __TIME__);
     cxxopts::Options options("SteamGiftsBot", "Bot for http://steamgifts.com/");
-    options.add_options()
-        ("h,help", "Print help")
-        ("c,cookie", "Auth cookie", cxxopts::value<std::string>())
-        ("config", "Path to config", cxxopts::value<std::string>()->default_value("data.ini"));
+    options.add_options()("h,help", "Print help")
+            ("c,cookie", "Auth cookie", cxxopts::value<std::string>())
+            ("config", "Path to config", cxxopts::value<std::string>()->default_value("data.ini")
+    );
     string phpsessid;
     string path;
     auto result = options.parse(argc, argv);
 
-
-    if(result.count("help")){
+    if (result.count("help"))
+    {
         printf("%s\n", options.help().c_str());
         return 0;
     }
 
-    if(result.count("config")){
-        path = result["config"].as<std::string>();
-    }else
+    if (result.count("config"))
     {
-        #ifdef SNAP_BUILD
+        path = result["config"].as<std::string>();
+    }
+    else
+    {
+#ifdef SNAP_BUILD
         auto env = std::getenv("SNAP_USER_DATA");
-        if(env == nullptr){
+        if (env == nullptr)
+        {
             printf("-> SNAP_USER_DATA env variable not found! Run it as snap!\n");
             return 1;
         }
         path = string(env) + string("/data.ini");
-        #else
+#else
         path = result["config"].as<std::string>();
-        #endif
+#endif
     }
 
-    
-    if(result.count("cookie")){
+    if (result.count("cookie"))
+    {
         phpsessid = result["cookie"].as<std::string>();
         WriteToFile(path, phpsessid);
-    }else if(!ReadFromFile(path, &phpsessid)){
+    }
+    else if (!ReadFromFile(path, &phpsessid))
+    {
         //printf("->Can't open file \"%s\"\n", path.c_str());
         return 0;
     }
 
-    SteamGiftAcc* acc = SteamGiftAcc::getInstance();
+    SteamGiftAcc *acc = SteamGiftAcc::getInstance();
 
     bool logged;
-    start:
-    try{
-    while ((logged = acc->log_in(phpsessid)))
+start:
+    try
     {
-        int entered = 0;
-        static int totalEntered = 0;
-        printf("Parsing data...\n");
+        while ((logged = acc->log_in(phpsessid)))
         {
-            auto giveaways(std::move(acc->parseGiveaways())); //parse first page
-            std::sort(giveaways.begin(), giveaways.end(),
-                      [](const GiveAway& ga1, const GiveAway& ga2){
-                          return ga1.getChancePercent() > ga2.getChancePercent();
-                      });
+            int entered = 0;
+            static int totalEntered = 0;
+            printf("Parsing data...\n");
+            {
+                auto giveaways(std::move(acc->parseGiveaways())); //parse first page
+                std::sort(giveaways.begin(), giveaways.end(),
+                          [](const GiveAway &ga1, const GiveAway &ga2) {
+                              return ga1.getChancePercent() > ga2.getChancePercent();
+                          });
 
-            printf("Parsed giveaways: %2zu\n", giveaways.size());
-            printf("Points available: %3d\n", acc->getPoints());
-            printf("Entering giveaways...\n");
-            for(const auto& ga : giveaways){
-                ERROR err = acc->enterGA(ga);
-                if(err == ERROR::NOT_ENOUGH_POINTS)
-                    break;
-                switch(err){
-                    case ERROR::OK:{
+                printf("Parsed giveaways: %2zu\n", giveaways.size());
+                printf("Points available: %3d\n", acc->getPoints());
+                printf("Entering giveaways...\n");
+                for (const auto &ga : giveaways)
+                {
+                    MYERROR err = acc->enterGA(ga);
+                    if (err == MYERROR::NOT_ENOUGH_POINTS)
+                        break;
+                    switch (err)
+                    {
+                    case MYERROR::OK:
+                    {
                         ++entered;
                         printf("[%40s] -- Price: %2d, Chance: %f%%\n", ga.name.c_str(), ga.price, ga.getChancePercent());
                         break;
                     }
-                    case ERROR::PREVIOUSLY_WON:{
+                    case MYERROR::PREVIOUSLY_WON:
+                    {
                         printf("[%40s] -- PREVIOUSLY_WON\n", ga.name.c_str());
                         break;
                     }
-                    case ERROR::UNKNOWN:{
+                    case MYERROR::UNKNOWN:
+                    {
                         printf("[%40s] -- ERROR\n", ga.name.c_str());
-                        printf("%s\n",static_cast<std::string>(ga).c_str());
+                        printf("%s\n", static_cast<std::string>(ga).c_str());
                         break;
+                    }
                     }
                 }
             }
+
+            printf("Entered: %3d\n", entered);
+            printf("Total:   %3d\n", totalEntered += entered);
+            time_t now = time(0);
+            tm* ltm = localtime(&now);
+            using time = std::chrono::system_clock;
+            time::to_time_t(time::now());
+            printf("%02d:%02d:%02d - sleeping for 15 mins.\n\n",
+                   ltm->tm_hour, ltm->tm_min, ltm->tm_sec);
+            std::this_thread::sleep_for(std::chrono::minutes(15));
         }
-        
-        printf("Entered: %3d\n", entered);
-        printf("Total:   %3d\n", totalEntered += entered);
-        time_t now = time(0);
-        tm* ltm = localtime(&now);
-        printf("%02d:%02d:%02d - sleeping for 15 mins.\n\n",
-               ltm->tm_hour, ltm->tm_min, ltm->tm_sec
-        );
-        std::this_thread::sleep_for(std::chrono::minutes(15));
-    }}
-    catch (std::runtime_error &err){
+    }
+    catch (std::runtime_error &err)
+    {
         printf("-> Caught error\nWhat(): %s\n", err.what());
         printf("-> Next try in 5 mins.");
         std::this_thread::sleep_for(std::chrono::minutes(5));
         goto start;
     }
-    if(!logged)
+    if (!logged)
         printf("-> Login failed.\n");
 
     printf("Exiting...\n");
     curl_global_cleanup();
-#ifdef WIN
-    // cout << "Press ENTER...";
-    system("pause");
-    cin.get();
-#endif
+
     return 0;
 }
 
-bool ReadFromFile(const string& path, string* lhs) {
-    try{
+bool ReadFromFile(const string &path, string *lhs)
+{
+    try
+    {
         // char buf[103 + 1];
         // ifstream f(path);
-        
+
         // if(!f.is_open()){
         //     char str[512];
         //     sprintf(str, "Can't open '%s'", path.c_str());
@@ -142,10 +170,11 @@ bool ReadFromFile(const string& path, string* lhs) {
         // f.close();
         // (*lhs) = std::string(buf);
         // if(strlen(buf) != 103)
-        //     throw std::runtime_error(R"(Insufficient lenght of contents of the file. 
+        //     throw std::runtime_error(R"(Insufficient lenght of contents of the file.
         //     Assuming the cookie must be exactly 103 char long.)");
         ifstream f(path);
-        if(!f.is_open()){
+        if (!f.is_open())
+        {
             char str[512];
             sprintf(str, "Can't open '%s'", path.c_str());
             throw std::runtime_error(str);
@@ -153,27 +182,36 @@ bool ReadFromFile(const string& path, string* lhs) {
         f >> (*lhs);
         f.close();
         return true;
-    }catch(std::runtime_error& err){
+    }
+    catch (std::runtime_error &err)
+    {
         printf("-> runtime_error. What(): %s\n", err.what());
-    }catch(...){
+    }
+    catch (...)
+    {
         printf("-> Unknown error.\n");
     }
     printFailFunction();
     return false;
 }
 
-bool WriteToFile(const string& path, const string& data){
-    try{
+bool WriteToFile(const string &path, const string &data)
+{
+    try
+    {
         ofstream f(path);
         f << data << std::endl;
         f.close();
         return true;
-    }catch(std::ofstream::failure& writeErr){
+    }
+    catch (std::ofstream::failure &writeErr)
+    {
         printf("-> What(): %s\n", writeErr.what());
-    }catch(...){
+    }
+    catch (...)
+    {
         printf("-> Unknown error.\n");
     }
     printFailFunction();
     return false;
 }
-    
